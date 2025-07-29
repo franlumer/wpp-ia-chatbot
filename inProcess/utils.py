d607c2e488dde73a43991fd695d107ed3ef92b98
franlumer <franlumer0@gmail.com>
2025-07-29 12:12:29 -0300

    Logica cambiada a POO y nuevas funciones agregadas a Context

--- inProcess/utils.py
+++ inProcess/utils.py
@@ -2,10 +2,15 @@ import re
 import credentials as cr
 from twilio.twiml.messaging_response import MessagingResponse
 
-chats = {} # numero : [mensajes]
+chats = {} # numero : [mensajes] | Solo para la version funcional, no para POO
+
+def limpiar_texto(IAResponseText):
+    msg = re.sub(r".*</think>", " ", IAResponseText, flags= re.DOTALL)
+    return msg
 
 class Context: 
     def __init__(self):
+        self.context = False
         self.messages = {}
 
     def add(self, incomingNum, incomingMsg = None, AIResponse = None):
@@ -18,9 +23,73 @@ class Context:
     def reset(self):
         self.messages = {}
 
-def limpiar_texto(IAResponseText):
-    msg = re.sub(r".*</think>", " ", IAResponseText, flags= re.DOTALL)
-    return msg
+    def start(self):
+        self.context = True
+
+    def stop(self):
+        self.context = False
+
+class Message:
+    def __init__(self):
+        pass
+
+    def check(self, message):
+            TwilioResponse = MessagingResponse()
+            match message: 
+                case None:
+                    TwilioResponse.message('Procesando...')
+                    checkMsg(message)
+                case _:
+                    TwilioResponse.message(message)
+
+    def send(self, message):
+        TwilioResponse = MessagingResponse()
+        TwilioResponse.message(message)
+        return TwilioResponse
+
+    def generate(self, incomingMsg, incomingNum, result_queue):
+        
+        try: 
+            IAresponse = cr.CLIENT.chat.completions.create(
+            model= cr.AI_MODEL,
+            messages = [{"role": "system", "content": cr.AI_PROMPT},
+            {"role": "user", "content": incomingMsg}])
+
+        except Exception as e:
+            result_queue.put(f'Error: {e}')    
+        
+        try:
+            match Context.context:
+                case True:
+                    Context.add(incomingNum, incomingMsg, limpiar_texto(IAresponse.choices[0].message.content))
+                case _: 
+                    pass
+
+        except Exception as e:
+            result_queue.put(f'Error (context): {e}')
+
+        IAResponseText = IAresponse.choices[0].message.content
+        msg = limpiar_texto(IAResponseText)
+        
+        result_queue.put(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def generateMsg(incomingMsg, incomingNum, result_queue):
     print(chats)
