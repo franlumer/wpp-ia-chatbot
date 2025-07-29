63a9075ef578af3f7b7d7d634aed747ed502b7d1
franlumer <franlumer0@gmail.com>
2025-07-29 12:12:07 -0300

    Logica cambiada a POO

--- inProcess/main.py
+++ inProcess/main.py
@@ -1,9 +1,7 @@
 from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 import threading
 import queue
 
-import credentials
 import utils
 # --------------------------------------------------------------------------------
 
@@ -13,24 +11,30 @@ app = Flask(__name__)
 
 def whatsapp_reply():
     incomingMsg = request.form.get('Body')
-    incomingNum = request.form.get('From') # returns whatsapp:+5493885107546
-    incomingNum = incomingNum.replace('whatsapp:+', '') # returns 5493885107546
+    incomingNum = request.form.get('From')               # returns whatsapp:+5493885107546
+    incomingNum = incomingNum.replace('whatsapp:+', '')  # returns 5493885107546
 
     match incomingMsg.lower():
-        case '/exit':
-            return str(utils.sendMessage("Cerrando sesion..."))
         
         case '/reset':
             utils.Context.reset()
-            return str(utils.sendMessage('Contexto reiniciado.'))
+            return str(utils.Message.send('*Contexto reiniciado.*'))
+        
+        case '/start':
+            utils.Context.start()
+            return str(utils.Message.send('*Contexto iniciado.*'))
             
+        case '/stop':
+            utils.Context.stop()
+            return(str(utils.Message.send('*Contexto detenido.*')))
+        
         case _:
             resultQueue = queue.Queue()
 
-            sesionThread = threading.Thread(target=utils.generateMsg, args=(incomingMsg, incomingNum, resultQueue))
+            sesionThread = threading.Thread(target=utils.Message.generate, args=(incomingMsg, incomingNum, resultQueue))
             sesionThread.start()
 
-            return str(utils.sendMessage(resultQueue.get()))
+            return str(utils.Message.send(resultQueue.get()))
 
 if __name__ == "__main__":
     app.run(port=8080, debug= True)
