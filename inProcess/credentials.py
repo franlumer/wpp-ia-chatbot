28c32bfcaf3302d5e4c1d27a87ae39df5c2e911b
franlumer <franlumer0@gmail.com>
2025-08-07 21:12:28 -0300

    -

--- inProcess/credentials.py
+++ inProcess/credentials.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 AI_MODEL = "deepseek/deepseek-r1-0528-qwen3-8b"
-BASE_URL ="http://192.168.50.101:8080/v1"
+BASE_URL = "http://192.168.50.101:8080/v1" 
 API_KEY ="not-needed"
 AI_PROMPT = "Eres un asistente útil y debes responder lo más rápido posible."
 
